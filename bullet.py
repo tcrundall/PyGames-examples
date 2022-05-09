dac82e6a9a623772902eb0bc02cf41cdee1d40a4
Timothy Crundall <u5018130@anu.edu.au>
2022-05-09 13:12:38 +0200

    Convert bullet to Class
    Bullet logic includes only position update
    and drawing

---
+++ bullet.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Bullet():
+    width = 15
+    height = 5
+    velx = 10
+
+    def __init__(self, color, x, y, direc, screen):
+        self.color = color
+        self.screen = screen
+        self.direc = direc
+        self.rect = pygame.Rect(x, y, self.width, self.height)
+    
+    def update(self):
+        # update position based on velocity
+        self.rect.x += self.direc * self.velx
+
+        # check for collision
+        # - maybe this can be handled by spaceships?
+
+        # check for leaving game boundary?
+        # - maybe this can be handled by 'GameRect'
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)        
